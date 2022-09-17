3f5a93644605369161349e36446cb3a33f32d77a
Fumblemunky <102485087+Fumblemunky@users.noreply.github.com>
2022-09-17 14:40:56 -0400

    Update SciFiProperties.js

--- SciFiProperties.js
+++ SciFiProperties.js
@@ -1,6 +1,17 @@
 Hooks.on('ready', () => {
 CONFIG.DND5E.weaponProperties['advanced'] = 'Advanced';
+CONFIG.DND5E.weaponProperties['bypass'] = 'Bypass';
+CONFIG.DND5E.weaponProperties['cavalry'] = 'Cavalry';
 CONFIG.DND5E.weaponProperties['energy'] = 'Energy';
+CONFIG.DND5E.weaponProperties['ensnaring'] = 'Ensnaring';
 CONFIG.DND5E.weaponProperties['explosive'] = 'Explosive';
+CONFIG.DND5E.weaponProperties['finisher'] = 'Finisher';
+CONFIG.DND5E.weaponProperties['gunpowder'] = 'Gunpowder';
+CONFIG.DND5E.weaponProperties['nonlethal'] = 'Nonlethal';
+CONFIG.DND5E.weaponProperties['parry'] = 'Parry';
+CONFIG.DND5E.weaponProperties['prone fighting'] = 'Prone Fighting';
 CONFIG.DND5E.weaponProperties['recharge'] = 'Recharge';
+CONFIG.DND5E.weaponProperties['skewering'] = 'Skewering';
+CONFIG.DND5E.weaponProperties['slicing'] = 'Slicing';
+CONFIG.DND5E.weaponProperties['staggering'] = 'Staggering';
 });

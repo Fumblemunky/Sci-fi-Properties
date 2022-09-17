122159c5eda41c1fcdaeb7868a9b728e63aca9c1
Fumblemunky <102485087+Fumblemunky@users.noreply.github.com>
2022-09-17 14:26:11 -0400

    Create SciFiProperties.js

---
+++ SciFiProperties.js
@@ -0,0 +1,6 @@
+Hooks.on('ready', () => {
+CONFIG.DND5E.weaponProperties['advanced'] = 'Advanced';
+CONFIG.DND5E.weaponProperties['energy'] = 'Energy';
+CONFIG.DND5E.weaponProperties['explosive'] = 'Explosive';
+CONFIG.DND5E.weaponProperties['recharge'] = 'Recharge';
+});
